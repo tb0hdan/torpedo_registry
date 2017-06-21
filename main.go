8eb2c6a9b557444198907f1b1ee55f78785984f3
Bohdan Turkynewych <tb0hdan@gmail.com>
2017-06-21 20:54:01 +0300

    moved RichMessage to registry

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package torpedo_registry
 
+type RichMessage struct {
+	BarColor  string
+	Text      string
+	Title     string
+	TitleLink string
+	ImageURL  string
+}
+
+func (rm *RichMessage) IsEmpty() bool {
+	return rm.Text == "" || rm.ImageURL == ""
+}
+
+func (rm *RichMessage) ToGenericAttachment() (msg, url string) {
+	msg = rm.Text
+	url = rm.ImageURL
+	return
+}
+
 type BotAPI struct {
 	API interface{}
 	Bot struct {
